11d67f471bfe041594d8e7a4f82cbcb886b2257d
Tim <sarrtim@gmail.com>
2018-11-20 22:13:51 +0300

    Add validations

--- skeleton/app/views.py
+++ skeleton/app/views.py
@@ -1,5 +1,24 @@
+from flask import request, jsonify
 from skeleton.app import app
+from skeleton.app.helpers import validator
+
 
 @app.route('/')
 def root():
     return "Welcome!"
+
+
+@app.route('/field/param', methods=['POST'])
+def create_field_params():
+    body = request.data
+    keys = ['fields']
+    if not body:
+        validated = validator.field_validator(keys, {})
+        if not validated["success"]:
+            return jsonify(validated['data'])
+    if request.is_json():
+        validated = validator.field_validator(keys, body)
+        if not validated["success"]:
+            return jsonify(validated['data'])
+    else:
+        return jsonify({"message": "Content type is not json"})

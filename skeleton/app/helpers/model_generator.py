c0fee9b5f9ffc7f315207f5a553b7de4cb14b05b
Tim <sarrtim@gmail.com>
2018-11-22 12:42:00 +0300

    Create dir from payload sent

--- skeleton/app/helpers/model_generator.py
+++ skeleton/app/helpers/model_generator.py
@@ -1,6 +1,24 @@
 import os
 
-file_path = os.getcwd()
+working_dir = os.getcwd()
+
+
+def create_model(data, file_path):
+    with open(file_path, 'w') as f:
+        for key in data:
+            print(key)
+
 
 def make_file(data):
-    pass
+    file_dir = '{}/app/{}/'.format(working_dir, data['dir'])
+    file_path = '{}/app/{}/{}'.format(working_dir, data['dir'], 'models.py')
+
+    try:
+        os.mkdir(file_dir)
+    except OSError:
+        print("Creation of the directory %s failed" % file_dir)
+    else:
+        print("Successfully created the directory %s" % file_dir)
+
+    create_model(data, file_path)
+    print('Execution completed.')

a4829a5a05505ab5eda6501fc76928e80f431e3f
Tim <sarrtim@gmail.com>
2018-11-22 11:39:06 +0300

    Add import for model generator helper and parse body to the make_file()

--- skeleton/app/views.py
+++ skeleton/app/views.py
@@ -1,6 +1,6 @@
 from flask import request, jsonify
 from skeleton.app import app
-from skeleton.app.helpers import validator
+from skeleton.app.helpers import validator, model_generator
 
 
 @app.route('/')
@@ -11,14 +11,16 @@ def root():
 @app.route('/field/param', methods=['POST'])
 def create_field_params():
     body = request.data
-    keys = ['fields']
+    keys = ['model', 'fields']
     if not body:
         validated = validator.field_validator(keys, {})
         if not validated["success"]:
             return jsonify(validated['data'])
     if request.is_json():
+        body = request.get_json()
         validated = validator.field_validator(keys, body)
         if not validated["success"]:
             return jsonify(validated['data'])
+        model_generator.make_file(body)
     else:
         return jsonify({"message": "Content type is not json"})

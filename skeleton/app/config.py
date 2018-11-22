ebfb3af10981194812e21eaace17cbf24a4bdbc6
Tim <sarrtim@gmail.com>
2018-11-22 12:12:18 +0300

    Cleanup config.py

--- skeleton/app/config.py
+++ skeleton/app/config.py
@@ -1,11 +1,5 @@
 import os
 
-from dotenv import load_dotenv
-
-dotenv_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../', '.env'))
-load_dotenv(dotenv_path)
-
-
 class BaseConfig(object):
     """
     Callable Base Config which takes an object

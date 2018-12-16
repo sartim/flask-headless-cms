c23cd4c96766d5283023c635b3bff7d886f9bc75
Tim <sarrtim@gmail.com>
2018-12-16 17:42:08 +0300

    Add installation requirements

--- setup.py
+++ setup.py
@@ -10,6 +10,16 @@ setup(name='FlaskHeadlessCms',
       packages=['flask_headless_cms'],
       install_requires=[
           'requests',
+          'Flask',
+          'Flask-Bcrypt',
+          'Flask-Cors',
+          'Flask-Migrate',
+          'Flask-Script',
+          'Flask-SQLAlchemy',
+          'gevent',
+          'greenlet',
+          'gunicorn',
+          'psycopg2'
       ],
       dependency_links=['https://github.com/sartim/flask-headless-cms/master#egg=package-1.0'],
       zip_safe=False)

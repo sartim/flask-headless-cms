418681b88c02efbce601b31e9fafef5eea5e2cd7
Tim <sarrtim@gmail.com>
2018-12-16 18:38:51 +0300

    Add extra arguments for db & inject template var to .env

--- flask_headless_cms/create_app.py
+++ flask_headless_cms/create_app.py
@@ -5,10 +5,17 @@ import os
 import argparse
 import subprocess
 import shutil
+import jinja2
+import codecs
 
 # Globals #
 cwd = os.getcwd()
 script_dir = os.path.dirname(os.path.realpath(__file__))
+
+# Jinja2 environment
+template_loader = jinja2.FileSystemLoader(
+    searchpath=os.path.join(script_dir, "templates"))
+template_env = jinja2.Environment(loader=template_loader)
 
 
 def get_arguments(argv):
@@ -16,7 +23,11 @@ def get_arguments(argv):
     parser.add_argument('appname', help='The application name')
     parser.add_argument('-s', '--skeleton', help='The skeleton folder to use.')
     parser.add_argument('-t', '--type', help='The database type to use')
-    parser.add_argument('-d', '--database', help='The database name to use')
+    parser.add_argument('-d', '--databasename', help='The database name to use')
+    parser.add_argument('-i', '--databasehost', help='The database host name to use')
+    parser.add_argument('-u', '--databaseuser', help='The database user name to use')
+    parser.add_argument('-p', '--databaseport', help='The database port to use')
+    parser.add_argument('-e', '--env', help='The environment to use')
     parser.add_argument('-g', '--git', action='store_true')
     args = parser.parse_args()
     return args
@@ -31,13 +42,37 @@ def main(args):
     fullpath = os.path.join(cwd, appname)
     skeleton_dir = args.skeleton
     database_type = args.type
-    database_name = args.database
+    db_name = args.databasename
+    db_host = args.databasehost
+    db_user = args.databaseuser
+    db_port = args.databaseport
+    env = args.env
 
     # Tasks #
 
     # Copy files and folders
     print("Copying files and folders...")
     shutil.copytree(os.path.join(script_dir, skeleton_dir), fullpath)
+
+    # Create config.py
+    print("Creating the config...")
+    secret_key = codecs.encode(os.urandom(32), 'hex').decode('utf-8')
+    basic_auth_password = codecs.encode(os.urandom(32), 'hex').decode('utf-8')
+    template = template_env.get_template('.env.jinja2')
+    template_var = {
+        'secret_key': secret_key,
+        'db_name': db_name,
+        'db_host': db_host,
+        'db_user': db_user,
+        'db_port': db_port,
+        'basic_auth_user': 'app',
+        'basic_auth_password': basic_auth_password,
+        'is_dev': 'TRUE' if env == 'dev' else 'FALSE',
+        'is_test': 'TRUE' if env == 'test' else 'FALSE',
+        'is_prod': 'TRUE' if env == 'prod' else 'FALSE'
+    }
+    with open(os.path.join(fullpath,'.env'), 'w') as fd:
+        fd.write(template.render(template_var))
 
     # Git init
     if args.git:

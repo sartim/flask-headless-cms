97c195f2a54311a318aa95441f974d1589b2540a
Tim <sarrtim@gmail.com>
2018-12-31 15:01:52 +0300

    Update setup.py to call find_packages()

--- setup.py
+++ setup.py
@@ -1,13 +1,16 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
-setup(name='FlaskHeadlessCms',
+NAME = 'FlaskHeadlessCms'
+
+setup(name=NAME,
       version='0.1',
       description='Flask Headless CMS',
       url='https://github.com/sartim/flask-headless-cms',
       author='sartim',
       author_email='sarrtim@gmail.com',
       license='MIT',
-      packages=['flask_headless_cms'],
+      packages=find_packages(),
+      include_package_data=True,
       install_requires=[
           'requests',
           'Flask',
@@ -23,6 +26,4 @@ setup(name='FlaskHeadlessCms',
           'psycopg2-binary',
           'whaaaaat',
           'prompt-toolkit==1.0.14'
-      ],
-      dependency_links=['https://github.com/sartim/flask-headless-cms/master#egg=package-1.0'],
-      zip_safe=False)
+      ])

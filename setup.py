0595e92381f463e2f36f57475a34517f9bae484d
Tim <sarrtim@gmail.com>
2018-12-31 17:27:24 +0300

    Append alias shortcut command flaskcli to bashrc on post-install

--- setup.py
+++ setup.py
@@ -1,8 +1,42 @@
+import os
+import re
+
 from setuptools import setup, find_packages
+from setuptools.command.develop import develop
+from setuptools.command.install import install
+from distutils.sysconfig import get_python_lib
 
-NAME = 'FlaskHeadlessCms'
+installed_path = '{path}/{name}'.format(path=get_python_lib(), name='flask_headless_cms')
+alias = 'alias flaskcli="python {0}/create_app.py"'.format(installed_path)
+pattern = re.compile(alias)
+homefolder = os.path.expanduser('~')
+bashrc = os.path.abspath('%s/.bashrc' % homefolder)
 
-setup(name=NAME,
+
+def appendToBashrc():
+  with open(bashrc, 'r') as f:
+    lines = f.readlines()
+    for line in lines:
+      if pattern.match(line):
+        return
+    out = open(bashrc, 'a')
+    out.write('\n%s' % alias)
+    out.close()
+
+
+class PostDevelopCommand(develop):
+    def run(self):
+        develop.run(self)
+        appendToBashrc()
+
+
+class PostInstallCommand(install):
+    def run(self):
+        install.run(self)
+        appendToBashrc()
+
+
+setup(name='FlaskHeadlessCms',
       version='0.1',
       description='Flask Headless CMS',
       url='https://github.com/sartim/flask-headless-cms',
@@ -27,4 +61,8 @@ setup(name=NAME,
           'psycopg2-binary',
           'whaaaaat',
           'prompt-toolkit==1.0.14'
-      ])
+      ],
+      cmdclass={
+          'develop': PostDevelopCommand,
+          'install': PostInstallCommand,
+      })

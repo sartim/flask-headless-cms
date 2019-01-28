72566fbb86192f3b50e2fc91fbe14f4f559fdae5
Tim <sarrtim@gmail.com>
2019-01-28 22:49:12 +0300

    Update postgres db_loader connection url

--- flask_headless_cms/db_loader/postgres.py
+++ flask_headless_cms/db_loader/postgres.py
@@ -6,9 +6,11 @@ def create(**kwargs):
     Create Database
     :return:
     """
-    engine = sqlalchemy.create_engine("postgres://{db_user}@{db_password}/{db_name}"
+    engine = sqlalchemy.create_engine("postgres://{db_user}:{db_password}@{db_host}:{db_port}/postgres"
                                       .format(db_user=kwargs['db_user'],
+                                              db_host=kwargs['db_host'],
                                               db_password=kwargs['db_password'],
+                                              db_port=kwargs['db_port'],
                                               db_name=kwargs['db_name']))
     conn = engine.connect()
     conn.execute("commit")

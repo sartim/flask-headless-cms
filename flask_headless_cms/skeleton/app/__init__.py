177571932f46bbb53b6580814c398f9259bf86e1
Tim <sarrtim@gmail.com>
2019-06-20 16:45:30 +0300

    Remove import and initialization for Flask Bcrypt

--- flask_headless_cms/skeleton/app/__init__.py
+++ flask_headless_cms/skeleton/app/__init__.py
@@ -1,5 +1,4 @@
 from flask import Flask
-from flask_bcrypt import Bcrypt
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 # from app.config import DevelopmentConfig
@@ -14,7 +13,6 @@ app = create_app()
 db = SQLAlchemy(app)
 db.init_app(app)
 CORS(app, supports_credentials=True)
-bcrypt = Bcrypt(app)
 
 if __name__ == "__main__":
     app.run(app)

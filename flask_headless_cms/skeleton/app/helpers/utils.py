59248ffc6f504df2f424e59871e7960daca8561e
Tim <sarrtim@gmail.com>
2019-06-21 19:55:50 +0300

    Remove else block

--- flask_headless_cms/skeleton/app/helpers/utils.py
+++ flask_headless_cms/skeleton/app/helpers/utils.py
@@ -6,9 +6,8 @@ working_dir = os.getcwd()
 def create_dir(dir):
     try:
         os.mkdir(dir)
-    except OSError:
-        print("Creation of the directory %s failed" % dir)
-    else:
         print("Successfully created the directory %s" % dir)
         return True
-    return False
+    except OSError:
+        print("Creation of the directory %s failed" % dir)
+        return False
